fcfebcbb7a4f8c75b42e65c9526960e403d5ef21
Plexdi <senpai200798@gmail.com>
2025-04-24 07:05:39 +0100

    more things to push yay

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+import authRouter from './api/index.js'
 
 const app = express(); // ✅ initialize express
 const Port = 5000;
@@ -9,11 +10,7 @@ app.use(cors());
 app.use(express.json()); // parse JSON body
 
 // ✅ Test Route
-app.post('/', async (req, res) => {
-  console.log('Request body:', req.body);
-  res.status(200).json({ message: 'Server received your request!' });
-});
-
+app.use('/api', authRouter)
 // ✅ Start the server
 app.listen(Port, () => {
   console.log(`🚀 Server running on http://localhost:${Port}`);
